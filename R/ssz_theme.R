6b278d25caabb1b8c9aad8f28ff2ff10afe4b36a
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-02-20 17:52:09 +0100

    style file

--- R/ssz_theme.R
+++ R/ssz_theme.R
@@ -19,139 +19,135 @@
 #' @importFrom grDevices rgb
 #' @examples \dontrun{
 #' ggplot(...) +
-#' ssz_theme()
+#'   ssz_theme()
 #' }
 ssz_theme <- function(base_size = 11,
-											base_family = "",
-											base_line_size = base_size / 170,
-											base_rect_size = base_size / 170,
-											ausrichtung = "x") {
-	# Werte müssen Character sein
-	if (!is.character(ausrichtung)) {
-		stop("ausrichtung muss character sein")
+                      base_family = "",
+                      base_line_size = base_size / 170,
+                      base_rect_size = base_size / 170,
+                      ausrichtung = "x") {
+  # Werte müssen Character sein
+  if (!is.character(ausrichtung)) {
+    stop("ausrichtung muss character sein")
 
-		# Grids, wenn ausrichtung == X
-	} else if (ausrichtung == "x") {
+    # Grids, wenn ausrichtung == X
+  } else if (ausrichtung == "x") {
+    # Minimales Theme anwenden
+    theme_minimal(
+      base_size = base_size,
+      base_family = base_family,
+      base_line_size = base_line_size
+    ) %+replace%
 
-		# Minimales Theme anwenden
-		theme_minimal(
-			base_size = base_size,
-			base_family = base_family,
-			base_line_size = base_line_size
-		) %+replace%
+      # SSZ-spezifische Theme-Eigenheiten
+      theme(
 
-			# SSZ-spezifische Theme-Eigenheiten
-			theme(
+        # Titel
+        plot.title = element_text(
+          color = rgb(25, 43, 65, maxColorValue = 255),
+          face = "bold",
+          hjust = 0
+        ),
+        plot.subtitle = element_text(
+          color = rgb(25, 43, 65, maxColorValue = 255),
+          face = "bold",
+          size = rel(0.5),
+          hjust = 0
+        ),
 
-				# Titel
-				plot.title = element_text(
-					color = rgb(25, 43, 65, maxColorValue = 255),
-					face = "bold",
-					hjust = 0
-				),
-				plot.subtitle = element_text(
-					color = rgb(25, 43, 65, maxColorValue = 255),
-					face = "bold",
-					size = rel(0.5),
-					hjust = 0
-				),
+        # Achsenbeschriftung
+        axis.title.x = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.75),
+          margin = margin(t = 20, r = 0, b = 0, l = 0)
+        ),
+        axis.title.y = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.75),
+          vjust = rel(1),
+          margin = margin(t = 0, r = 20, b = 0, l = 0)
+        ),
+        axis.text = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.55)
+        ),
 
-				# Achsenbeschriftung
-				axis.title.x = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.75),
-					margin = margin(t = 20, r = 0, b = 0, l = 0)
-				),
-				axis.title.y = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.75),
-					vjust = rel(1),
-					margin = margin(t = 0, r = 20, b = 0, l = 0)
-				),
-				axis.text = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.55)
-				),
+        # Gitterlinien
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(rgb(105, 105, 105, maxColorValue = 255),
+          linetype = "solid",
+          size = rel(0.75)
+        ),
+        panel.grid.minor = element_blank(),
 
-				# Gitterlinien
-				panel.grid.major.x = element_blank(),
-				panel.grid.major.y = element_line(rgb(105, 105, 105, maxColorValue = 255),
-																					linetype = "solid",
-																					size = rel(0.75)
-				),
-				panel.grid.minor = element_blank(),
+        # Legende
+        legend.position = "right",
+        legend.title = element_blank(),
+        legend.text = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.75)
+        ),
+        complete = TRUE
+      )
 
-				# Legende
-				legend.position = "right",
-				legend.title = element_blank(),
-				legend.text = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.75)
-				),
+    # Grids, wenn ausrichtung == Y
+  } else if (ausrichtung == "y") {
+    # Minimales Theme anwenden
+    theme_minimal(
+      base_size = base_size,
+      base_family = base_family,
+      base_line_size = base_line_size
+    ) %+replace%
 
-				complete = TRUE
-			)
+      # SSZ-spezifische Theme-Eigenheiten
+      theme(
 
-		# Grids, wenn ausrichtung == Y
-	} else if (ausrichtung == "y") {
+        # Titel
+        plot.title = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          face = "bold",
+          hjust = 0
+        ),
+        plot.subtitle = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          face = "bold",
+          size = rel(0.5),
+          hjust = 0
+        ),
 
-		# Minimales Theme anwenden
-		theme_minimal(
-			base_size = base_size,
-			base_family = base_family,
-			base_line_size = base_line_size
-		) %+replace%
+        # Achsenbeschriftung
+        axis.title.x = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.75),
+          margin = margin(t = 20, r = 0, b = 0, l = 0)
+        ),
+        axis.title.y = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.75),
+          vjust = rel(1),
+          margin = margin(t = 0, r = 20, b = 0, l = 0)
+        ),
+        axis.text = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.55)
+        ),
 
-			# SSZ-spezifische Theme-Eigenheiten
-			theme(
+        # Gitterlinien
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(rgb(105, 105, 105, maxColorValue = 255),
+          linetype = "solid",
+          size = rel(0.75)
+        ),
+        panel.grid.minor = element_blank(),
 
-				# Titel
-				plot.title = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					face = "bold",
-					hjust = 0
-				),
-				plot.subtitle = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					face = "bold",
-					size = rel(0.5),
-					hjust = 0
-				),
-
-				# Achsenbeschriftung
-				axis.title.x = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.75),
-					margin = margin(t = 20, r = 0, b = 0, l = 0)
-				),
-				axis.title.y = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.75),
-					vjust = rel(1),
-					margin = margin(t = 0, r = 20, b = 0, l = 0)
-				),
-				axis.text = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.55)
-				),
-
-				# Gitterlinien
-				panel.grid.major.y = element_blank(),
-				panel.grid.major.x = element_line(rgb(105, 105, 105, maxColorValue = 255),
-																					linetype = "solid",
-																					size = rel(0.75)
-				),
-				panel.grid.minor = element_blank(),
-
-				# Legende
-				legend.position = "right",
-				legend.title = element_blank(),
-				legend.text = element_text(
-					color = rgb(105, 105, 105, maxColorValue = 255),
-					size = rel(0.75)
-				),
-
-				complete = TRUE
-			)
-	}
+        # Legende
+        legend.position = "right",
+        legend.title = element_blank(),
+        legend.text = element_text(
+          color = rgb(105, 105, 105, maxColorValue = 255),
+          size = rel(0.75)
+        ),
+        complete = TRUE
+      )
+  }
 }
