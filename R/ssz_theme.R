f3c78ca452108b2848d1897933e497a194a1089e
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-08-23 16:57:28 +0200

    also remove common stuff for y

--- R/ssz_theme.R
+++ R/ssz_theme.R
@@ -85,74 +85,15 @@ ssz_theme <- function(base_size = 8.2,
 		# List object
 		list(
 			# Base theme_bw
-			theme_bw(
+			ssz_theme_base(
 				base_size = base_size,
 				base_family = base_family,
-				base_line_size = base_line_size
+				base_line_size = base_line_size,
+				base_rect_size = base_rect_size
 			) %+replace%
 
 				# SSZ specific theme components
 				theme(
-					# Title
-					plot.title.position = "plot",
-
-					plot.title = element_text(
-						color = "#020304",
-						face = "bold",
-						size = rel(1.5),
-						hjust = 0,
-						margin = margin(
-							t = 0,
-							r = 0,
-							b = 2,
-							l = 0
-						)
-					),
-
-					# Subtitle
-					plot.subtitle = element_text(
-						color = "#020304",
-						hjust = 0,
-						size = rel(1),
-						margin = margin(
-							t = 0,
-							r = 0,
-							b = 25,
-							l = 0
-						)
-					),
-
-					# Axis text and title
-					axis.title.x = element_text(
-						color = "#020304",
-						size = rel(1),
-						hjust = 0,
-						margin = margin(
-							t = 10,
-							r = 0,
-							b = 0,
-							l = 0
-						)
-					),
-
-					axis.title.y = element_text(
-						color = "#020304",
-						size = rel(1),
-						hjust = 0,
-						vjust = 1,
-						margin = margin(
-							t = 0,
-							r = 0,
-							b = 0,
-							l = 0
-						)
-					),
-
-					axis.text = element_text(color = "#020304",
-																	 size = rel(1)),
-
-					axis.ticks = element_blank(),
-
 					# Grid Lines
 					panel.grid.major.x = element_blank(),
 
@@ -161,62 +102,6 @@ ssz_theme <- function(base_size = 8.2,
 						linetype = "solid",
 						size = rel(3)
 					),
-
-					panel.grid.minor = element_blank(),
-
-					panel.border = element_blank(),
-
-					panel.spacing = unit(base_size / 50, "lines"),
-
-					# Legend
-					legend.position = "left",
-
-					legend.justification = c(0, 1),
-
-					legend.margin = margin(t = -2, r = 100, b = 0, l = 0),
-
-					legend.title = element_blank(),
-
-					legend.text = element_text(color = "#020304",
-																		 size = rel(1)),
-
-					legend.key.height = unit(base_size / 11, "line"),
-
-					legend.key.width = unit(base_size / 11, "line"),
-
-					legend.spacing.y = unit(base_size / 100, "line"),
-
-					# Facets
-					strip.background = element_blank(),
-
-					strip.text = element_text(
-						color = "#020304",
-						face = "bold",
-						size = rel(1),
-						hjust = 0,
-						margin = margin(
-							t = 0,
-							r = 0,
-							b = 5,
-							l = 0
-						),
-					),
-
-					# Caption
-					plot.caption.position = "plot",
-
-					plot.caption = element_text(
-						size = rel(1),
-						margin = margin(
-							t = 15,
-							r = 0,
-							b = 0,
-							l = 0
-						),
-						hjust = 0
-					),
-
-					complete = FALSE
 				),
 
 			guides(fill = guide_legend(byrow = TRUE))
