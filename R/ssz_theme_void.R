aa37c7227b8e2a6fa89df3921df60a82b629b472
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-10-03 17:34:22 +0200

    Styling

--- R/ssz_theme_void.R
+++ R/ssz_theme_void.R
@@ -16,106 +16,99 @@
 #'   ssz_theme_void()
 #' }
 ssz_theme_void <- function(base_size = 8.2,
-													 base_family = "",
-													 base_line_size = base_size / 170,
-													 base_rect_size = base_size / 170) {
-	# List object
+                           base_family = "",
+                           base_line_size = base_size / 170,
+                           base_rect_size = base_size / 170) {
+  # List object
 
-		# Base theme_bw
-		theme_void(
-			base_size = base_size,
-			base_family = base_family,
-			base_line_size = base_line_size
-		) %+replace%
+  # Base theme_bw
+  theme_void(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size
+  ) %+replace%
 
-			# SSZ specific theme components
-			theme(
-				# Title
-				plot.title.position = "plot",
+    # SSZ specific theme components
+    theme(
 
-				plot.title = element_text(
-					color = "#020304",
-					face = "bold",
-					size = rel(1.5),
-					hjust = 0,
-					margin = margin(
-						t = 0,
-						r = 0,
-						b = 2,
-						l = 0
-					)
-				),
+      # Title
+      plot.title.position = "plot",
+      plot.title = element_text(
+        color = "#020304",
+        face = "bold",
+        size = rel(1.5),
+        hjust = 0,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 2,
+          l = 0
+        )
+      ),
 
-				# Subtitle
-				plot.subtitle = element_text(
-					color = "#020304",
-					size = rel(1),
-					hjust = 0,
-					margin = margin(
-						t = 0,
-						r = 0,
-						b = 25,
-						l = 0
-					)
-				),
+      # Subtitle
+      plot.subtitle = element_text(
+        color = "#020304",
+        size = rel(1),
+        hjust = 0,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 25,
+          l = 0
+        )
+      ),
 
-				# Panel
-				panel.spacing = unit(base_size / 11, "lines"),
+      # Panel
+      panel.spacing = unit(base_size / 11, "lines"),
 
-				# Plot Background
-				plot.background = element_rect(fill = "white", color = NA),
+      # Plot Background
+      plot.background = element_rect(fill = "white", color = NA),
 
-				# Legend
-				legend.position = "left",
+      # Legend
+      legend.position = "left",
+      legend.justification = c(0, 1),
+      legend.margin = margin(
+        t = -2,
+        r = 100,
+        b = 0,
+        l = 0
+      ),
+      legend.title = element_blank(),
+      legend.text = element_text(
+        color = "#020304",
+        size = rel(1)
+      ),
+      legend.key.height = unit(base_size / 11, "line"),
+      legend.key.width = unit(base_size / 11, "line"),
 
-				legend.justification = c(0, 1),
+      # Facets
+      strip.background = element_blank(),
+      strip.text = element_text(
+        color = "#020304",
+        face = "bold",
+        size = rel(1),
+        hjust = 0,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 5,
+          l = 0
+        ),
+      ),
 
-				legend.margin = margin(
-					t = -2,
-					r = 100,
-					b = 0,
-					l = 0
-				),
-
-				legend.title = element_blank(),
-
-				legend.text = element_text(color = "#020304",
-																	 size = rel(1)),
-
-				legend.key.height = unit(base_size / 11, "line"),
-
-				legend.key.width = unit(base_size / 11, "line"),
-
-				# Facets
-				strip.background = element_blank(),
-
-				strip.text = element_text(
-					color = "#020304",
-					face = "bold",
-					size = rel(1),
-					hjust = 0,
-					margin = margin(
-						t = 0,
-						r = 0,
-						b = 5,
-						l = 0
-					),
-				),
-
-				# Caption
-				plot.caption.position = "plot",
-
-				plot.caption = element_text(
-					size = rel(1),
-					margin = margin(
-						t = 15,
-						r = 0,
-						b = 0,
-						l = 0
-					),
-					hjust = 0
-				),
-
-				complete = FALSE
-			)
+      # Caption
+      plot.caption.position = "plot",
+      plot.caption = element_text(
+        size = rel(1),
+        margin = margin(
+          t = 15,
+          r = 0,
+          b = 0,
+          l = 0
+        ),
+        hjust = 0
+      ),
+      complete = FALSE
+    )
 }
