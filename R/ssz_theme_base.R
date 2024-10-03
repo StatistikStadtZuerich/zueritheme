5b7abc61cfd6d4b65b5cf86976a9bc205bae517c
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-10-03 17:34:16 +0200

    Implemented new web theme

--- R/ssz_theme_base.R
+++ R/ssz_theme_base.R
@@ -10,132 +10,121 @@
 #'
 #' @return a ggplot theme
 ssz_theme_base <- function(base_size,
-													 base_family,
-													 base_line_size,
-													 base_rect_size) {
+                           base_family,
+                           base_line_size,
+                           base_rect_size) {
+  # Base theme_bw
+  theme_bw(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size
+  ) %+replace%
 
-	# Base theme_bw
-	theme_bw(
-		base_size = base_size,
-		base_family = base_family,
-		base_line_size = base_line_size
-	) %+replace%
+    # SSZ specific theme components
+    theme(
 
-		# SSZ specific theme components
-		theme(
+      # Title
+      plot.title.position = "plot",
+      plot.title = element_text(
+        color = "#020304",
+        face = "bold",
+        size = rel(1.5),
+        hjust = 0,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 2,
+          l = 0
+        )
+      ),
 
-			# Title
-			plot.title.position = "plot",
+      # Subtitle
+      plot.subtitle = element_text(
+        color = "#020304",
+        size = rel(1),
+        hjust = 0,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 10,
+          l = 0
+        )
+      ),
 
-			plot.title = element_text(
-				color = "#020304",
-				face = "bold",
-				size = rel(1.5),
-				hjust = 0,
-				margin = margin(
-					t = 0,
-					r = 0,
-					b = 2,
-					l = 0
-				)
-			),
+      # Axis text and title
+      axis.title.x = element_text(
+        color = "#020304",
+        size = rel(1),
+        hjust = 0,
+        margin = margin(
+          t = 10,
+          r = 0,
+          b = 0,
+          l = 0
+        )
+      ),
+      axis.title.y = element_text(
+        color = "#020304",
+        size = rel(1),
+        hjust = 0,
+        vjust = 1,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 0,
+          l = 0
+        )
+      ),
+      axis.text = element_text(
+        color = "#020304",
+        size = rel(1)
+      ),
+      axis.ticks = element_blank(),
 
-			# Subtitle
-			plot.subtitle = element_text(
-				color = "#020304",
-				size = rel(1),
-				hjust = 0,
-				margin = margin(
-					t = 0,
-					r = 0,
-					b = 10,
-					l = 0
-				)
-			),
+      # Panel Grid
+      panel.grid.minor = element_blank(),
+      panel.border = element_blank(),
+      panel.spacing = unit(base_size / 11, "lines"),
 
-			# Axis text and title
-			axis.title.x = element_text(
-				color = "#020304",
-				size = rel(1),
-				hjust = 0,
-				margin = margin(
-					t = 10,
-					r = 0,
-					b = 0,
-					l = 0
-				)
-			),
+      # Legend
+      legend.position = "left",
+      legend.justification = c(0, 1),
+      legend.margin = margin(t = -2, r = base_size * 2, b = 0, l = 0),
+      legend.title = element_blank(),
+      legend.text = element_text(
+        color = "#020304",
+        size = rel(1)
+      ),
+      legend.key.height = unit(base_size / 11, "line"),
+      legend.key.width = unit(base_size / 11, "line"),
 
-			axis.title.y = element_text(
-				color = "#020304",
-				size = rel(1),
-				hjust = 0,
-				vjust = 1,
-				margin = margin(
-					t = 0,
-					r = 0,
-					b = 0,
-					l = 0
-				)
-			),
+      # Facets
+      strip.background = element_blank(),
+      strip.text = element_text(
+        color = "#020304",
+        face = "bold",
+        size = rel(1),
+        hjust = 0,
+        margin = margin(
+          t = 0,
+          r = 0,
+          b = 5,
+          l = 0
+        ),
+      ),
 
-			axis.text = element_text(color = "#020304",
-															 size = rel(1)),
-
-			axis.ticks = element_blank(),
-
-			panel.grid.minor = element_blank(),
-
-			panel.border = element_blank(),
-
-			panel.spacing = unit(base_size / 11, "lines"),
-
-			# Legend
-			legend.position = "left",
-
-			legend.justification = c(0, 1),
-
-			legend.margin = margin(t = -2, r = base_size*2, b = 0, l = 0),
-
-			legend.title = element_blank(),
-
-			legend.text = element_text(color = "#020304",
-																 size = rel(1)),
-
-			legend.key.height = unit(base_size / 11, "line"),
-
-			legend.key.width = unit(base_size / 11, "line"),
-
-			# Facets
-			strip.background = element_blank(),
-
-			strip.text = element_text(
-				color = "#020304",
-				face = "bold",
-				size = rel(1),
-				hjust = 0,
-				margin = margin(
-					t = 0,
-					r = 0,
-					b = 5,
-					l = 0
-				),
-			),
-
-			# Caption
-			plot.caption.position = "plot",
-
-			plot.caption = element_text(
-				size = rel(1),
-				margin = margin(
-					t = 15,
-					r = 0,
-					b = 0,
-					l = 0
-				),
-				hjust = 0
-			),
-
-			complete = FALSE
-		)
+      # Caption
+      plot.caption.position = "plot",
+      plot.caption = element_text(
+        size = rel(1),
+        margin = margin(
+          t = 15,
+          r = 0,
+          b = 0,
+          l = 0
+        ),
+        hjust = 0
+      ),
+      complete = FALSE
+    )
 }
