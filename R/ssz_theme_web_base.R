144613d8d1ff92a47e66b0158639facf386e9164
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-10-08 14:08:36 +0200

    Updated function documentation

--- R/ssz_theme_web_base.R
+++ R/ssz_theme_web_base.R
@@ -1,12 +1,22 @@
 #' ssz_theme_base_web
 #'
-#' this creates a ggplot theme that can be used as a base theme on top of which to
-#' replace the items that depend on the orientation of the plot
+#' @description Function that creates a ggplot theme that can be used as a base
+#' theme on top of which to replace the theme's items that depend on the orientation
+#' of the web plot.
 #'
-#' @param base_size optional, basic font size, parameter passed to minimal theme
-#' @param base_family optional, basic font family, parameter to be passed to minimal theme
-#' @param base_line_size optional, basic line size, parameter passed to minimal theme
-#' @param base_rect_size optional, basic rect size, parameter passed to minimal theme
+#' @param base_size optional, defines the base font size used throughout the plot.
+#' This parameter is passed to the theme_bw function, ensuring consistent typography
+#' across ggplot elements.
+#' @param base_family optional, specifies the base font family for the text elements
+#' in the ggplot object. This parameter is passed to the theme_bw function, ensuring
+#' a consistent font style across ggplot elements.
+#' @param base_line_size optional, sets the base line size for lines and borders
+#' within the ggplot object. This parameter is passed to the theme_bw function, ensuring
+#' consistent line thickness across visual elements.
+#' @param base_rect_size optional, specifies the base size for rectangular elements
+#' such as legend boxes or plot backgrounds within the ggplot object. This parameter
+#' is passed to the theme_bw function, ensuring consistent sizing of rectangular
+#' shapes across ggplot elements.
 #'
 #' @return a ggplot theme
 ssz_theme_web_base <- function(base_size,
