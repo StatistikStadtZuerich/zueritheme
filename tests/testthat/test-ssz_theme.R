e455a1f6186b3b3ad9cb8cbfaaa4c1e5edf0eb28
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2025-04-02 15:11:36 +0200

    implement super-minimal tests

---
+++ tests/testthat/test-ssz_theme.R
@@ -0,0 +1,9 @@
+test_that("ssz_theme returns a ggplot theme", {
+	expect_s3_class(ssz_theme(grid_lines = "x", publication_type = "print"),
+									c("theme", "gg"))
+})
+
+test_that("ssz_theme returns errors if incorrect parameters specified", {
+	expect_error(ssz_theme(grid_lines = "a"))
+	expect_error(ssz_theme(publication_type = "something"))
+})
